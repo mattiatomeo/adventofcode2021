3ade0da3a9a017de5bd0ca07d8d7a97a4f917239
Mattia Tomeo <tomeo.mat@gmail.com>
2021-12-05 00:00:17 +0100

    add step 1 and assert in day1 and update names

--- day1/count_increase.py
+++ day1/count_increase.py
@@ -1,8 +1,8 @@
-def count_increase(measures):
+def depth_increase_1(measures):
     return sum(int(measures[i] < measures[i + 1]) for i in range(len(measures) - 1))
 
 
-def count_window_increase(measures):
+def depth_increase_2(measures):
     return sum(int(measures[i + 3] > measures[i]) for i in range(len(measures) - 3))
 
 
@@ -10,7 +10,8 @@ def main():
     with open("input.txt", "r") as fp:
         measures = [int(value) for value in fp.readlines()]
 
-    print(count_window_increase(measures))
+    assert depth_increase_1(measures) == 1475
+    assert depth_increase_2(measures) == 1516
 
 
 if __name__ == '__main__':
