cb0b01d236c9c46f2951ea9ae1a341e24d511909
Mattia Tomeo <tomeo.mat@gmail.com>
2021-12-11 23:18:08 +0100

    look on all available position in day7
    find_cheapest_position check any candidate position, because I am not sure that every local minimum is a global minimum

--- day7/crabs.py
+++ day7/crabs.py
@@ -1,30 +1,21 @@
 import numpy as np
 
-from typing import Callable
+from typing import Callable, List
 
 
 def read_crabs_positions() -> np.array:
     with open("input.txt", "r") as f:
-        return np.array([int(pos) for pos in f.readline().split(',')])
+        return list(map(int, f.readline().split(',')))
 
 
-def find_cheapest_position(crabs_positions: np.array, distance: Callable[[int, int], int]) -> int:
-    curr_position = crabs_positions.min()
+def find_cheapest_position(crabs_positions: List[int], distance: Callable[[int, int], int]) -> int:
+    min_pos = min(crabs_positions)
+    max_pos = max(crabs_positions)
 
-    vect_dist_f = np.vectorize(distance)
+    def fuel_consumption(position: int) -> int:
+        return sum(distance(crab, position) for crab in crabs_positions)
 
-    curr_pos_value = vect_dist_f(crabs_positions, curr_position).sum()
-    curr_position += 1
-    while curr_position < crabs_positions.max():
-        next_position_value = vect_dist_f(crabs_positions, curr_position).sum()
-
-        if next_position_value > curr_pos_value:
-            break
-
-        curr_pos_value = next_position_value
-        curr_position += 1
-
-    return curr_pos_value
+    return min(fuel_consumption(pos) for pos in range(min_pos, max_pos + 1))
 
 
 if __name__ == '__main__':
